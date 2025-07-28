e99481131a44144090d4f9d4e88e6ded04066ed0
A3therium <74322088+A3therium@users.noreply.github.com>
2025-07-28 13:56:08 +0100

    Added overworld nether fog

---
+++ src/client/java/com/a3utils/mixin/client/DimensionEffectsNetherMixin.java
@@ -0,0 +1,14 @@
+package com.a3utils.mixin.client;
+
+import net.minecraft.client.render.DimensionEffects.Nether;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Nether.class)
+public class DimensionEffectsNetherMixin {
+    @Overwrite
+    public boolean useThickFog(int camX, int camY) {
+        return false;
+    }
+}
