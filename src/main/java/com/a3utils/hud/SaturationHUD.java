da17b4eb7aa8a76ac0e5bf9ce665bc585507603a
A3therium <samkoseyhorner@gmail.com>
2026-02-05 20:53:11 +0000

    Adedd background to saturation hud

--- src/main/java/com/a3utils/hud/SaturationHUD.java
+++ src/main/java/com/a3utils/hud/SaturationHUD.java
@@ -6,6 +6,8 @@ public class SaturationHUD {
     private static final float MAX_WIDTH = 81;
 
     public static void render(GuiGraphics context, int top, int right, float saturation) {
+        context.fill(right - (int) MAX_WIDTH, top + 9, right, top + 10, 0xFF000000);
+
         int left = right - (int) (MAX_WIDTH * (saturation / 20));
         context.fill(left, top + 9, right, top + 10, 0xFFFF0000);
     }
